2da3976fc742b343ea17ea5d505fea3622cda7b1
Ramirez Stwart <ramirezstwart@gmail.com>
2025-05-19 13:14:41 -0300

    AJUSTE-API2

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ import packageInfo from '../../package.json';
 export const environment = {
   appVersion: packageInfo.version,
   production: true,
-  apiUrl: 'http://127.0.0.1:2000' // Coloque o IP do seu PC aqui!
+  apiUrl: 'http://192.168.99.51:2000' // Coloque o IP do seu PC aqui!
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@ import packageInfo from '../../package.json';
 export const environment = {
   appVersion: packageInfo.version,
   production: true,
-  apiUrl: 'http://127.0.0.1:2000' // Link api RENDER
+  apiUrl: 'http://192.168.99.51:2000' // Link api RENDER
 };
 
 
