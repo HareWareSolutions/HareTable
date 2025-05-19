04b882b2dbd2bb7a7d4b0bf8aef6d79c507cd557
Ramirez Stwart <ramirezstwart@gmail.com>
2025-05-19 14:56:29 -0300

    https-ajuste

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ import packageInfo from '../../package.json';
 export const environment = {
   appVersion: packageInfo.version,
   production: true,
-  apiUrl: 'http://192.168.99.51:2000' // Coloque o IP do seu PC aqui!
+  apiUrl: ' https://backend.haretable.com.br' // Coloque o IP do seu PC aqui!
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@ import packageInfo from '../../package.json';
 export const environment = {
   appVersion: packageInfo.version,
   production: true,
-  apiUrl: 'http://192.168.99.51:2000' // Link api RENDER
+  apiUrl: 'https://backend.haretable.com.br' // Link api RENDER
 };
 
 
